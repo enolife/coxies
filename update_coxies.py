646cb451247402c4ae19e14e9a17d903e15af86f
enolife <35650337+enolife@users.noreply.github.com>
2024-09-28 22:26:31 +0100

    Update update_coxies.py

--- update_coxies.py
+++ update_coxies.py
@@ -1,34 +1,34 @@
 import os
 import requests
 
-def update_proxies_two():
-    proxies_url = "https://tinyurl.com/y7xvaru7"
-    response = requests.get(proxies_url)
+def update_coxies_two():
+    coxies_url = "https://tinyurl.com/y7xvaru7"
+    response = requests.get(coxies_url)
 
     # Print the current working directory
     current_dir = os.getcwd()
     print(f"Current working directory: {current_dir}")
 
     lines = response.text.strip().split('\n')
-    proxies = [f"socks5://{line.split(',')[0]}" for line in lines[1:] if line.strip()]
+    coxies = [f"socks5://{line.split(',')[0]}" for line in lines[1:] if line.strip()]
 
-    proxies = proxies[:100000]
+    coxies = coxies[:100000]
 
-    # Write proxies to file
-    socks5_proxies_path = os.path.join(current_dir, "coxies.txt")
-    print(f"Writing proxies to: {socks5_proxies_path}")
+    # Write coxies to file
+    socks5_coxies_path = os.path.join(current_dir, "coxies.txt")
+    print(f"Writing coxies to: {socks5_coxies_path}")
 
-    with open(socks5_proxies_path, "a") as f:
-        f.write("\n".join(proxies))
+    with open(socks5_coxies_path, "w") as f:
+        f.write("\n".join(coxies))
         f.write("\n")
-        print("Proxies updated successfully")
+        print("coxies updated successfully")
 
     # Clean up empty lines
-    with open(socks5_proxies_path, "r") as f:
+    with open(socks5_coxies_path, "r") as f:
         lines = f.readlines()
 
-    with open(socks5_proxies_path, "w") as f:
+    with open(socks5_coxies_path, "w") as f:
         f.writelines(line for line in lines if line.strip())
 
 if __name__ == "__main__":
-    update_proxies_two()
+    update_coxies_two()
