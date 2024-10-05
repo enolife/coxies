25eff6884b1e2d26679937d1c960382c93b8944b
enolife <35650337+enolife@users.noreply.github.com>
2024-10-05 20:29:24 +0100

    Update update_coxies.py

--- update_coxies.py
+++ update_coxies.py
@@ -2,8 +2,26 @@ import os
 import requests
 
 def update_coxies_two():
+    headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": '"Microsoft Edge";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
+}
+
     coxies_url = "https://tinyurl.com/y7xvaru7"
-    response = requests.get(coxies_url, allow_redirects=True, verify=False)
+    response = requests.get(coxies_url, headers = headers, allow_redirects=True, verify=False)
     if response.status_code != 200:
         print(f"Failed to retrieve data: {response.status_code}")
         return
